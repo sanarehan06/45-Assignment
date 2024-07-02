49049393dfe8f0ab25e8f936eab866e02091b5df
SanaRehan <161079696+sanarehan06@users.noreply.github.com>
2024-07-02 15:17:40 +0500

    Add files via upload

---
+++ Assignment 2-10/main.ts
@@ -0,0 +1,68 @@
+//Assignment number: 4
+
+let famQuote:string = "A person who never made a mistake never tried anything new.";
+let author:string = "Albert Einstein";
+
+console.log(`${author} once said,"${famQuote}"`);
+
+//Assignment number : 5
+
+let famousPerson:string = "Albert Einstien";
+let message : string = `${famousPerson} once said,"${famQuote}"`;
+
+console.log(message);
+
+//Assignment number : 6
+
+let nameWithWhiteSpace : string = "\t \n Ayesha Noor \t \n";
+console.log("name with white space:",nameWithWhiteSpace);
+
+let strippedName:string= nameWithWhiteSpace.trim();
+console.log("stripped Name:", 
+strippedName);
+
+//Assignment number : 7
+
+let num1:number= 2;
+let num2:number= 4;
+let num3:number= 10;
+let num4:number=16;
+
+console.log(num2+num2)
+console.log(num3-num1)
+console.log(num2*num1)
+console.log(num4/num1)
+
+//Assignment number : 8
+console.log(5+3)
+console.log(10-2)
+console.log(2*4)
+console.log(32/4)
+
+//Assignment number : 9
+let favouriteNumber:number =6;
+console.log("My favourite number is"+" "+ favouriteNumber +".")
+
+//ctrl + questions
+
+//Assignment number : 10
+
+//this program runs unary operators
+
+//define numbers
+let numa: number= 2;
+let numb:number= 4;
+let numc: number= 10;
+let numd:number=16;
+
+//unary operators and print the answers = 8
+console.log(num2+num2)
+console.log(num3-num1)
+console.log(num2*num1)
+console.log(num4/num1)
+
+
+
+
+
+
